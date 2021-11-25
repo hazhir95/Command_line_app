5b8ec3ff8cf61c5cb095d639b1ecaef326259679
Hajir Rasouli <54510090+hazhir95@users.noreply.github.com>
2021-11-25 02:59:55 -0500

    Delete src/com/company/exp directory

--- src/com/company/exp/Class_1.java
+++
@@ -1,9 +0,0 @@
-package com.company.exp;
-
-public class Class_1 {
-    void bark(){
-        Main fun = new Main();
-        System.out.println("woof-woof");
-        fun.menu(); // Reload (recall) menu method from Main Class.
-    }
-}
